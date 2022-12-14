d9ac00d588a0806f3bdcfd1757273c72dd8985ac
powcoder <powcoder@163.com>
2022-12-14 12:34:24 +0800

    CS570 HW4

---
+++ stub/Maze/GridColors.java
@@ -0,0 +1,25 @@
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+https://powcoder.com
+代写代考加微信 powcoder
+Assignment Project Exam Help
+Add WeChat powcoder
+package Maze;
+
+import java.awt.Color;
+
+/**
+ *  An interface for colors
+ *@author Koffman and Wolfgang
+ */
+public interface GridColors {
+
+    Color PATH = Color.green;
+    Color BACKGROUND = Color.white;
+    Color NON_BACKGROUND = Color.red;
+    Color ABNORMAL = NON_BACKGROUND;
+    Color TEMPORARY = Color.black;
+}
+/*</exercise>*/
